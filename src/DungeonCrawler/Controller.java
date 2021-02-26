73e72aa95f5588dd6742580c9a55724131b83627
Chuong Dong <cdong49@gatech.edu>
2021-02-25 21:49:08 -0500

    Set up config screen

--- src/DungeonCrawler/Controller.java
+++ src/DungeonCrawler/Controller.java
@@ -4,17 +4,52 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.stage.Stage;
 
 public class Controller extends Application {
+    private Stage primaryStage;
+    private final int width = 500;
+    private final int height = 500;
+    private String characterName = "";
+    private String difficulty = "";
+    private String weapon = "";
+
+
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("DungeonCrawler.fxml"));
-        primaryStage.setTitle("Dungeon Crawler");
-        primaryStage.setScene(new Scene(root, 300, 275));
-        primaryStage.show();
+        this.primaryStage = primaryStage;
+        this.primaryStage.setTitle("Dungeon Crawler");
+        initConfigScreen(); // Comment this out when you test your screen.
     }
 
+    private void initConfigScreen() {
+        ConfigScreen configScreen = new ConfigScreen(width, height);
+        Button proceedButton = configScreen.getProceedButton();
+        proceedButton.setOnAction(e -> {
+            if (configScreen.getNameField().getText().length() == 0) {
+                return;
+            }
+            this.characterName = configScreen.getNameField().getText();
+            if (configScreen.getDifficultyDropdown().getValue() == null) {
+                return;
+            }
+            this.difficulty = configScreen.getDifficultyDropdown().getValue().toString();
+            if (configScreen.getWeaponDropdown().getValue() == null) {
+                return;
+            }
+            this.weapon = configScreen.getWeaponDropdown().getValue().toString();
+            System.out.println(this.difficulty);
+            System.out.println(this.weapon);
+            proceedToGameScreen();
+        });
+        this.primaryStage.setScene(configScreen.getScene());
+        this.primaryStage.show();
+    }
+
+    private void proceedToGameScreen() {
+        // TODO: create the initial game scene class, add that scene here, setScene for the stage, call show to change screen
+    }
 
     public static void main(String[] args) {
         launch(args);
