97adeabedcc83ce20f662cb86918096bf8743d35
Tristan Rogers <trogers64@gatech.edu>
2021-03-14 18:29:38 -0400

    Added tests for starting adjacent rooms.

--- test/DungeonCrawlerTest.java
+++ test/DungeonCrawlerTest.java
@@ -272,5 +272,57 @@ public class DungeonCrawlerTest extends ApplicationTest {
         clickOn("#dogeButton");
         verifyThat("#exitButton", NodeMatchers.isEnabled());
     }
+
+    @Test
+    public void testStartingLeftRoom() {
+        clickOn("Start");
+        clickOn("#nameField").write("Tristan");
+        clickOn("#easyRB");
+        clickOn("#weaponDropdown");
+        clickOn("Shortsword");
+        clickOn("PROCEED");
+        clickOn("left");
+        Text roomID = lookup("#id").queryText();
+        assertEquals(roomID.getText(), "left");
+    }
+
+    @Test
+    public void testStartingRightRoom() {
+        clickOn("Start");
+        clickOn("#nameField").write("Tristan");
+        clickOn("#easyRB");
+        clickOn("#weaponDropdown");
+        clickOn("Shortsword");
+        clickOn("PROCEED");
+        clickOn("right");
+        Text roomID = lookup("#id").queryText();
+        assertEquals(roomID.getText(), "right");
+    }
+
+    @Test
+    public void testStartingUpRoom() {
+        clickOn("Start");
+        clickOn("#nameField").write("Tristan");
+        clickOn("#easyRB");
+        clickOn("#weaponDropdown");
+        clickOn("Shortsword");
+        clickOn("PROCEED");
+        clickOn("up");
+        Text roomID = lookup("#id").queryText();
+        assertEquals(roomID.getText(), "up");
+    }
+
+    @Test
+    public void testStartingDownRoom() {
+        clickOn("Start");
+        clickOn("#nameField").write("Tristan");
+        clickOn("#easyRB");
+        clickOn("#weaponDropdown");
+        clickOn("Shortsword");
+        clickOn("PROCEED");
+        clickOn("down");
+        Text roomID = lookup("#id").queryText();
+        assertEquals(roomID.getText(), "down");
+    }
 }
 
