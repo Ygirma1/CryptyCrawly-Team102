3dd8914b3627e302278084d08108d04f799747a4
Chuong Dong <cdong49@gatech.edu>
2021-03-28 17:50:32 -0400

    small player fix

--- src/dungeoncrawler/Player.java
+++ src/dungeoncrawler/Player.java
@@ -14,7 +14,6 @@ public class Player extends Rectangle {
     private boolean goWest;
     private static boolean alive = true;
     private boolean isAggressive;
-    private static boolean alive;
     public Player(double x, double y, int width, int height) {
         super(x, y, width, height);
         this.setVisible(true);
